f014422870ba14a1d8dd89b50a6ed765e8f21762
AntonyDrift <42956301+AntonyDrift@users.noreply.github.com>
2018-11-03 11:57:42 +0300

    Update and rename NameSurname.java to Lesson-13/NameSurname.java

--- Lesson-13/NameSurname.java
+++ Lesson-13/NameSurname.java
@@ -2,8 +2,6 @@ package com.company;
 
 public class NameSurname {
 
-
-
     public String fio (String surname, String firstname, String patronymic){
         return surname.toUpperCase().charAt(0)+"."+firstname.toUpperCase().charAt(0)+"."+patronymic.toUpperCase().charAt(0);
 
